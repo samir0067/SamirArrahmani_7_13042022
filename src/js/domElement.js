8c9919ddabf0a02ea3bc84c2aff1db5fe4a03bf1
samir <arrahmani.samir@gmail.com>
2022-05-04 15:59:47 +0200

    wip

---
+++ src/js/domElement.js
@@ -0,0 +1,24 @@
+export const searchBar = document.getElementById("searchBar")
+export const main = document.querySelector(".main")
+export const items = document.querySelectorAll(".items")
+export const tags = document.getElementById("tags")
+
+export const itemIngredients = document.querySelectorAll(".items_ingredient")
+export const itemAppliances = document.querySelectorAll(".items_appliance")
+export const itemUtensils = document.querySelectorAll(".items_utensils")
+
+export const inputIngredient = document.getElementById("inputIngredient")
+export const inputAppliance = document.getElementById("inputAppliance")
+export const inputUtensils = document.getElementById("inputUtensils")
+
+export const dropdownSubContainerIngredients = document.querySelector(".dropdown_subContainer_ingredients")
+export const dropdownSubContainerAppliances = document.querySelector(".dropdown_subContainer_appliances")
+export const dropdownSubContainerUtensils = document.querySelector(".dropdown_subContainer_utensils")
+
+export const dropdownIngredient = document.querySelector(".dropdown_ingredient")
+export const dropdownAppliance = document.querySelector(".dropdown_appliance")
+export const dropdownUtensils = document.querySelector(".dropdown_utensils")
+
+export const inputContentIngredient = document.querySelector(".input_content_ingredient")
+export const inputContentAppliance = document.querySelector(".input_content_appliance")
+export const inputContentUtensils = document.querySelector(".input_content_utensils")
